7cbc5fe86245fca4604fd05df0b11458dffbc246
Amira Lee <amiralee97@gmail.com>
2023-01-25 19:43:31 -0500

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 const overviewElement = document.querySelector(".overview");
 const username = "amiralee97";
+const repoListElement = document.querySelector(".repo-list");
 
 // pulling info from my github repos
 const gitHubProfile = async function () {
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
     UserInfoDisplay(data)
-    console.log(data);
+    // console.log(data);
 };
 gitHubProfile();
 
@@ -25,4 +26,21 @@ const UserInfoDisplay = function (data) {
     </div>`
 
 overviewElement.append(div);
+repoList();
+};
+
+const repoList = async function() {
+    const repoFetch = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoFetch.json();
+    displayInfo(repoData);
+};
+
+
+const displayInfo = function(repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoListElement.append(repoItem);
+}
 };
